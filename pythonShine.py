a87d180a69c6e37d69028f41a3d853b5b644dc11
landalex <AlexLand@d207-023-189-202.wireless.sfu.ca>
2013-11-28 14:17:54 -0800

    Misc fixes and alpha catastrophe generator

--- pythonShine.py
+++ pythonShine.py
@@ -18,62 +18,87 @@ catastropheNumber = 0
 pythonShine = ""
 playerName = ""
 startGame = ""
-playerHealth = ""
+playerHealth = 0
 catastrophe = ""
-maximumTurns = ""
+maximumTurns = 0
 turnNumber = 0
 playerSword = 0
 playerDiamonds = 0
-gameOn = False
+roundOn = False
+playerPosInput = ""
+gameOn = True
+gameOnInput = ""
+catastrophePos = ""
+biomeList = []
 
 # FUNCTIONS
 
 def gameStart():
     global startGame
-    startGame = ""
+    counter = 0
     while startGame != "y":
         startGame = raw_input("Would you like to play a game (y/n)? ")
+        if startGame != "y":
+            print "Please enter y to play."
+            counter += 1
+        if counter >= 5:
+            print "Think you're funny? You're not."
         
     global godMode
     while godMode == "":
         godModeInput = raw_input("Would you like to manually choose biomes (y/n)? ")
-        if godModeInput == "y":
+        if godModeInput == "y" or godModeInput == "yes":
             godMode = True
-        elif godModeInput == "n":
+        elif godModeInput == "n" or godModeInput == "no":
             godMode = False
         else:
             print "Please enter y for yes or n for no."
 
 def enterInfo():
     global playerHealth
-    if godMode == True:
-        playerHealth = input("What is your initial health? (Pick a number between 10 and 50) ")
-    if godMode == False:
-        playerHealth = 10* (random.randint(1,5))
+    while playerHealth < 10 or playerHealth > 50:
+        if godMode == True:
+            playerHealth = input("What is your initial health? (Pick a number between 10 and 50) ")
+            if playerHealth < 10 or playerHealth > 50:
+                print "Please pick a value between 10 and 50."
+        if godMode == False:
+            playerHealth = 10* (random.randint(1,5))
         
     global playerName
     while playerName == "":
         playerName = raw_input("What is the name of your character? ")
         
     global maximumTurns
-    maximumTurns = input("How many turns do you want to play (maximum)? ")
+    while maximumTurns <= 0:
+        maximumTurns = input("How many turns do you want to play (maximum)? ")
+        if maximumTurns <= 0:
+            print "Please input a value greater than 0."
 
     global playerPos
     playerPos = 0
 
-    global gameOn
-    gameOn = True
+    global roundOn
+    roundOn = True
 
     
     
 def turnGenerator():
     global playerPos
     global turnNumber
+    global playerPosInput
     if turnNumber >= 0:
         if godMode == True:
             playerPos = input("Which biome would you like to go to (Pick a number between 1 and 7)? ")
+            while playerPos <= 0 or playerPos > 7:
+                print "Please input a value between 1 and 7."
+                playerPos = input("Which biome would you like to go to (Pick a number between 1 and 7)? ")
         if godMode == False:
-            playerPos = random.randint(1,7)
+            playerPosInput = raw_input("Are you ready for the next turn? (y/n) ")
+            if playerPosInput == "y":
+                playerPos = ((playerPos + random.randint(1,6)) % 8)
+            elif playerPosInput != "y":
+                print "Tricked you, there was never a choice."
+                playerPos = ((playerPos + random.randint(1,6)) % 8)
         
     turnNumber += 1
     print "Starting turn number", turnNumber
@@ -152,6 +177,7 @@ def biomeDataParser(localList):
     global biomeDiamonds
     global biomeSwords
     global biomeEnemies
+    global biomeList
     for i in range(len(localList)):                 #wow, much comments
         biomeData.append(localList[i].split("-"))    # many biome lists, wow. 
     for j in range(len(biomeData)):
@@ -159,32 +185,53 @@ def biomeDataParser(localList):
             biomeDiamonds.append(int(biomeData[j][k]))
             biomeSwords.append(int(biomeData[j][k+1]))
             biomeEnemies.append(int(biomeData[j][k+2]))
-    
+            biomeList.append(j)
 
 def biomeTable():
     print "The current board state is as follows:"
     print "Biome Diamd Sword Enemy"
-    for i in range(len(biomeData)):
-        if pythonShine == i:
-            print i , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
-              biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], "  <=== PythonShine"
-        elif playerPos == i and turnNumber >= 0:
-            print i , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
-                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], " <===", playerName
-        elif playerPos == i and turnNumber < 0:
-            print i , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+    for i in range(len(biomeList)):
+        if godMode == True:
+            if pythonShine == i:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], "  <=== PythonShine"
+            elif playerPos == i and turnNumber >= 0:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                    biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], " <===", playerName
+            elif playerPos == i and turnNumber < 0:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                      biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
+            elif pythonShine == playerPos:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], "  <=== PythonShine", " <===", playerName
+            else:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
                   biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
-        elif pythonShine == i and playerName == i:
-            print "You're in PythonShine!"
-        else:
-            print i , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
-              biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
+        if godMode == False:
+            if pythonShine == i:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
+            elif playerPos == i and turnNumber >= 0:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                      biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], " <===", playerName
+            elif playerPos == i and turnNumber < 0:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                      biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
+            elif pythonShine == playerPos:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i], "  <=== PythonShine", " <===", playerName
+            else:
+                print biomeList[i] , " " * (5-(len(str(i)))), biomeDiamonds[i], " " * (5-(len(str(biomeDiamonds[i])))), \
+                  biomeSwords[i], " " * (5-(len(str(biomeSwords[i])))), biomeEnemies[i]
 
 def pythonShineGenerator():
     global pythonShine
     if godMode == True:
-        pythonShine = input("Where do you want to place PythonShine? (Pick a number from 2 to 8) ")
-        pythonShine += -1
+        while pythonShine < 1 or pythonShine > 7:
+            pythonShine = input("Where do you want to place PythonShine? (Pick a number from 1 to 7) ")
+            if pythonShine < 1 or pythonShine > 7:
+                print "Please input a value between 1 and 7."
+            
     elif godMode == False:
         pythonShine = random.randint(1, 7)
 
@@ -192,7 +239,9 @@ def combatCalculator():
     global playerSword
     global playerHealth
     if biomeSwords[playerPos] > playerSword:
+        tempSword = playerSword
         playerSword = biomeSwords[playerPos]
+        biomeSwords[playerPos] = tempSword
         print playerName, "has picked up a level", playerSword, "sword!"
     if playerSword > biomeEnemies[playerPos]:
         healthGain = random.randint(1, playerHealth)
@@ -217,44 +266,96 @@ def diamondCalculator():
         print playerName, "has collected", (biomeDiamonds[playerPos]/3), "diamonds!"
 
 def playerInfo():
-    print playerName + " currently has:"
-    print playerHealth, "health."
-    print playerDiamonds, "diamonds."
-    if playerSword == 0:
-        print "No sword."
-    else:
-        "A level", playerSword, "sword."
-    if playerPos == "":
-        print "And is about to spawn."
-    else:
-        print "And is in position", playerPos
-    
-def gameCheck():
+    global playerSword
     global gameOn
-    #if playerPos == pythonShine:
-       # gameOn = False
-    if playerHealth <= 0:
-        gameOn = False
-    if turnNumber == maximumTurns:
-        gameOn = False
-    
+    global gameOnInput
+    if roundOn == True:
+        print playerName + " currently has:"
+        print playerHealth, "health."
+        print playerDiamonds, "diamonds."
+        if playerSword == 0:
+            print "No sword."
+        else:
+            "A level", playerSword, "sword."
+        if playerPos == -1:
+            print "And is about to spawn."
+        else:
+            print "And is in position", playerPos
+            
+    elif roundOn == False:
+        print "Game Over!"
+        if playerHealth <= 0:
+            print playerName + "has died."
+        elif playerDiamonds == 9999:
+            print playerName + "has won!"
+        elif turnNumber == maximumTurns:
+            print "The maximum number of turns has been reached."
+        print playerName + " ended the game with:"
+        print playerHealth, "health."
+        print playerDiamonds, "diamonds."
+        if playerSword == 0:
+            print "No sword."
+        else:
+            "A level", playerSword, "sword."
 
+    if gameOn == True:
+        gameOnInput = raw_input("Would you like to play another game? (y/n) ")
+        while gameOnInput != "y" or gameOnInput != "n":
+            print "Please input y for yes or n for no."
+        if gameOnInput == "y":
+            gameOn = True
+        if gameOnInput == "n":
+            gameOn = False
+
+            
+def gameCheck():
+    global roundOn
+    if playerPos == pythonShine:
+        roundOn = False
+    if playerHealth <= 0:
+        roundOn = False
+    if turnNumber == maximumTurns:
+        roundOn = False
+    
+def catastropheGenerator():
+    global catastrophePos
+    global catastropheNumber
+    catastrophePos = (7 / 5) * (random.randint(1,7))
+    for i in range(len(biomeList)):
+        if catastrophePos == biomeList[i]:
+            catastropheNumber += 1
+            print "A catastrophe has occured in biome", catastrophePos
+            print "Biome", catastrophePos, "has been destroyed!"
+            biomelist.remove(catastrophePos)
+            if playerPos == catastrophePos:
+                playerHealth == 0
+                print playerName, "has been caught in the catastrophe!"
+                
 # EXECUTION TOP LEVEL
 
 gameStart()
 readFile("biomesData1.txt")
 biomeDataParser(localList)
 pythonShineGenerator()
+print 
 biomeTable()
+print 
 enterInfo()
+
 
 # WHILE LOOP
 while gameOn == True:
-    biomeTable()
-    playerInfo()
-    turnGenerator()
-    diamondCalculator()
-    combatCalculator()
-    gameCheck()
+    while roundOn == True:
+        print 
+        biomeTable()
+        print 
+        playerInfo()
+        print 
+        turnGenerator()
+        diamondCalculator()
+        combatCalculator()
+        catastropheGenerator()
+        gameCheck()
 
-playerInfo()
+    print 
+    playerInfo()
--- pythonShineTest.py
+++ pythonShineTest.py
@@ -1,18 +1,17 @@
-localList = ["0-0-0", "10-0-1", "10-1-3"]
-biomeDiamonds = []
-biomeSwords = []
-biomeEnemies = []
-biomeData = []
-for i in range(len(localList)):
-    biomeData.append(localList[i].split("-"))
-for j in range(len(biomeData)):
-    for k in range(1):
-        biomeDiamonds.append(biomeData[j][k])
-        biomeSwords.append(biomeData[j][k+1])
-        biomeEnemies.append(biomeData[j][k+2])
+newList = []
+for i in range(len(listOrig)):
+	newList.append(listOrig[i])
 
+counter = 0
 
-print biomeData
-print biomeDiamonds
-print biomeSwords
-print biomeEnemies
+for i in range(len(listOrig) - 1):
+	counter += 1
+	if listOrig[i] > listOrig[i+1]:
+		newList[i+1] = newList[i]
+		newList[i] = listOrig[i+1]
+	elif listOrig[i] <= listOrig[i+1]:
+		newList[i+1] = listOrig[i+1]
+	if counter == ((len(listOrig)) - 1):
+		if newList[-2] > newList[-1]:
+			newList[-1] = newList[-2]
+			newList[-2] = listOrig[-1]
